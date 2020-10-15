374903ee3efd55a439e2746c7503726e72532db9
capulusnoctis <axel.solares.g@gmail.com>
2020-10-14 22:55:33 -0600

    Added: Exercise with structures and pointers to structures

--- struct.c
+++ struct.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(){
+int main(int argc, const char * argv[]) {
+	struct {
+		char name[100];
+		int age;
+	} person;
+
+	if (argc < 3) {
+		printf("Indicar nombre y edad, por favor\n");
+
+		return 1;
+	}
+
+	if ( strlen(argv[1]) < 100 ) {
+		strcpy( person.name, argv[1] );
+	}
+
+	person.age = atoi(argv[2]);
+
+	printf("Name = %s, age = %d\n", person.name, person.age);
+
 	return 0;
 }
