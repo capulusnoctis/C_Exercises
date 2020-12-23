d9cdbc0e84841c01960465ca4eef96c61a16d719
sekosolares <axel.solares.g@gmail.com>
2020-12-23 03:11:22 -0600

    Added: Client socket exercise

---
+++ client.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <sys/sockets.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int main (int argc, const char * argv[]) {
+
+	if (argc > 2) {
+		char * ip;
+		int client_socket, numbytes, puerto;
+		char buff[100];
+		puerto = atoi( argv[2] );
+		ip = argv[1];
+
+		struct sockaddr_in server;
+
+		if ( inet_pton(AF_INET, argv[1], &server.sin_addr) <= 0 ) {
+			perror("La ip %s no es valida", ip);
+
+			return -1;
+		}
+		printf("Abriendo el socket\n");
+
+		if ( ( client_socket = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 ) {
+			perror("No pude abrir socket\n");
+
+			return -2;
+		}
+
+		server.sin_family = AF_INET;
+		server.sin_port = htons( puerto );
+		bzero( &(server.sin_zero), 8 );
+
+		printf("Conectando a %s:%d", argv[1], argv[2]);
+
+		if ( connect( client_socket, (struct sockaddr *)&server, sizeof(struct sockaddr) ) == -1 ) {
+			perror("No se pudo conectar al servidor\n");
+
+			return -3;
+		}
+
+		printf("Recibiendo...\n");
+
+		if ( (numbytes = recv(client_socket, buff, 100, 0)) == -1 ) {
+			printf("Error en la lectura\n");
+
+			return -4;
+		}
+
+		buff[numbytes] = '\0';
+
+		printf("El servidor envio el mensaje '%s'\n", buff);
+
+		shutdown(client_socket, 2);
+	} else {
+		printf("Por favor, indique puerto!\n");
+
+		return -5;
+	}
+
+	return 0;
+}
